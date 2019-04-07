f0d9101e286d99b2d4cb4558395d6f454ae47e72
朋也 <py2qiuse@gmail.com>
2019-04-07 08:04:29 +0800

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -14,7 +14,7 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <h1 className="App-title">朋也项目集合</h1>
+          <h1 className="App-title">朋也项目集合</h1>
         </header>
         <div className="app-body">
           <div className="row">
